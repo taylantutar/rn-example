333859441222c5b91ef242d7fd53fadc670d9c79
Taylan Tutar <tylntutar@gmail.com>
2024-08-20 22:24:16 +0300

    use state and click events

--- workshop/src/screens/LoginPage.js
+++ workshop/src/screens/LoginPage.js
@@ -4,28 +4,66 @@ import {
   Text,
   StyleSheet,
   View,
-  SafeAreaView
+  SafeAreaView,
+  TextInput,
+  Pressable
 } from 'react-native'
-import React, { Component } from 'react'
+import React,{ useState } from 'react'
 
-export default class LoginPage extends Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.AndroidSafeArea}>
-
-        <View>
-          <Text>Login Page</Text>
-        </View>
-
-      </SafeAreaView>
-    )
-  }
+function LoginButton(){
+  console.log("Clicked")
 }
+
+export default function LoginPage() {
+
+  const [email, setEmail] = useState("");
+  return (
+    <SafeAreaView style={styles.AndroidSafeArea}>
+
+      <View style={styles.container}>
+        <Text>Hello: {email}</Text>
+        <TextInput style={styles.customInput} placeholder='Lütfen adinizi giriniz..'
+          value={email}
+          onChangeText={setEmail}></TextInput>
+
+        <Pressable style={styles.saveButton} onPress={() => LoginButton()}>
+          <Text>Kaydet</Text>
+        </Pressable>
+      </View>
+
+    </SafeAreaView>
+  )
+}
+
+
 
 const styles = StyleSheet.create({
   AndroidSafeArea: {
     flex: 1,
     backgroundColor: "white",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  customInput: {
+    borderColor: "gray",
+    borderRadius: 16,
+    borderWidth: 3,
+    width: "80%",
+    height: 40,
+    textAlign: "center",
+    marginTop: 10
+  },
+  saveButton: {
+    backgroundColor: "lightblue",
+    width: "80%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginTop: 10
   }
 })
